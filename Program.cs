38e4b383d1824bd498e4b9e816e2e4b4e4922495
Anna_Zagrebina <eto-projdet@yandex.ru>
2023-02-16 23:06:56 +0300

    Add programm

--- Program.cs
+++ Program.cs
@@ -1,15 +1,41 @@
-﻿String[] array = new String[]{"Это", " лето", "кастаньет", " на", "лазурном", "побережье"," !"};
-
-String[] ArrayWith3LengthStrings (String[] arr)
+﻿String[] array = new String[] { "Это", " лето", "кастаньет", " на", "лазурном", "побережье", "!" };
+int size2Array = NewArrayLength(array);
+if (size2Array > 0)
 {
-    String[] array3Lengh = new String[arr.Length];
-    int j = 1;
+    String[] newArray = ArrayWith3LengthStrings(array, size2Array);
+    PrintArray(array);
+    Console.Write(" -> ");
+    PrintArray(newArray);
+}
+else
+{
+    PrintArray(array);
+    Console.Write(" -> []");
+}
+
+
+int NewArrayLength(String[] arr)
+{
+    int j = 0;
     for (int i = 0; i < arr.Length; i++)
     {
-        String str = arr[i];
-        if (str.Length <= 3)
+        if (array[i].Length <= 3)
         {
-            array3Lengh[j] = str;
+            j++;
+        }
+    }
+    return j;
+}
+
+String[] ArrayWith3LengthStrings(String[] arr, int size)
+{
+    String[] array3Lengh = new String[size];
+    int j = 0;
+    for (int i = 0; i < arr.Length; i++)
+    {
+        if (arr[i].Length <= 3)
+        {
+            array3Lengh[j] = arr[i];
             j++;
         }
     }
@@ -17,3 +43,12 @@ String[] ArrayWith3LengthStrings (String[] arr)
     return array3Lengh;
 }
 
+void PrintArray(String[] arr)
+{
+    Console.Write("[");
+    for (int i = 0; i < arr.Length - 1; i++)
+    {
+        Console.Write($"\"{arr[i],2}\",");
+    }
+    Console.Write($"\"{arr[arr.Length - 1]}\"]");
+}

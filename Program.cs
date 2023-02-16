cd14a2be749637934da5645edf0de120b8555a1a
Anna_Zagrebina <eto-projdet@yandex.ru>
2023-02-16 22:04:13 +0300

    Add start of programm

--- Program.cs
+++ Program.cs
@@ -1,2 +1,19 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿String[] array = new String[]{"Это", " лето", "кастаньет", " на", "лазурном", "побережье"," !"};
+
+String[] ArrayWith3LengthStrings (String[] arr)
+{
+    String[] array3Lengh = new String[arr.Length];
+    int j = 1;
+    for (int i = 0; i < arr.Length; i++)
+    {
+        String str = arr[i];
+        if (str.Length <= 3)
+        {
+            array3Lengh[j] = str;
+            j++;
+        }
+    }
+
+    return array3Lengh;
+}
+
